3aa57ea202c869e9f8538381766fea147b7191f9
Jose Francisco <napsterck@gmail.com>
2020-10-04 13:19:52 -0500

    Update main.c
    esta nueva version del codigo hace una rampa gradual de encendido con voltaje y frecuencia en rampa de 5 segundos. se observa que la rampa genera cortos de las senoidales lo que proboca un frenado del rotor no deseado.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -68,7 +68,8 @@
 /* Private define ------------------------------------------------------------*/
 
 #define pi 3.1415926 // se define el valor de Pi en este programa
-#define step 10		// se asignan los pasos de cambio de frecuencia
+#define step 2.4		// se asignan los pasos de cambio de frecuencia
+
 
 
 
@@ -76,7 +77,9 @@
 float32_t w, f, sine_step = 0; //variables para almacenar los valores de la frecuencia en radianes y el valor del PWM
 uint16_t counter = 0;		   // contador para saber cuanto desplazar el valor de X de la senoidal de 0 a 2pi
 uint16_t pwm_sine1, pwm_sine2 = 0;// valores a cargar en los PWM para el sigeuinte disparo de PWM 1 y 3
+float32_t atenuacion = 0; // variable de crecimiento de amplitud de la senoidal de salida
 
+_Bool ON = 0;// variable de encendido de rampa
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
@@ -90,7 +93,9 @@ int main(void)
   SystemClock_Config();
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_TIM2_Init();
 
+HAL_TIM_Base_Start_IT(&htim2);// configuracion del timer 2 como interrupcion para la rampa de encendido
 
 HAL_TIM_PWM_Start_IT(&htim1, TIM_CHANNEL_1); // configuracion de canal 1 del timer1 como salida PWM NO negada y ademas como interrupcion
 HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);// configuracion de la salida PWM negada del canal 1
@@ -153,11 +158,11 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)// interrupcion por accion del bot
 {
 
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, SET); // encendemos en LED de señalizacion mientras el programa entre en esta interrupcion.
-
-	f = f + step ;// incrementamos los valores de la frencuencia 10 Hz.
-	if(f >= 120)
+	ON = !ON;
+	if(ON == SET)
 	{
-		f = 0; // si f alcanza una frecuencia de 120 Hz regresa a una frecuencia de 0 Hz
+		f = 12;//frecuencia inicial
+		atenuacion = 0.2;
 	}
 }
 
@@ -168,11 +173,11 @@ void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
 	sine_step = ( w / 10333 ); // si el PWM es de 10.33 kHz, calcula cuanto debe de avanzar x en radianes para que cada segundo se de la frecuencia w
 
 	pwm_sine1 = (uint16_t) (2046 / 2) * ( arm_sin_f32( counter * sine_step ) + 1 ); // se calcula el valor de la señal senoidal sin desafase
-	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwm_sine1);// se carga el nuevo valor de la señal seno en el registro PWM para su activacion.
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (pwm_sine1 * atenuacion));// se carga el nuevo valor de la señal seno en el registro PWM para su activacion.
 
 	w = w + pi; // se hace el desafase de la señal 180° o un valor de pi radianes
 	pwm_sine2 = (uint16_t) (2046 / 2) * ( arm_sin_f32( (counter * sine_step) + pi ) + 1 );// se calcula el valor de la señal senoidal con desafase
-	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, pwm_sine2);// se carga el nuevo valor de la señal seno en el registro PWM para su activacion.
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, (pwm_sine2 * atenuacion));// se carga el nuevo valor de la señal seno en el registro PWM para su activacion.
 
 	counter++; // se salta un paso para que la siguiente interrupcion se valcule un nuevo valor de la senoidal
 	if(counter > 10333)
@@ -188,6 +193,51 @@ void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
 
 }
 
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+//	bandera de señalizacio para saber cuando entra la interrupcion
+	HAL_GPIO_WritePin(FLAG_GPIO_Port, FLAG_Pin, SET);
+	HAL_GPIO_WritePin(FLAG_GPIO_Port, FLAG_Pin, RESET);
+
+	//rutina para el cambio de la freucuecia en la rampa
+	if (ON == SET)
+	{
+//		generamos la rampa de frecuencia y voltaje en ascenso (proceso de encendido)
+
+		f = f + step ;// incrementamos los valores de la frencuencia 2.4 Hz.
+		if(f >= 60)
+		{
+			f = 60; // si f alcanza una frecuencia de 60 Hz quedate en una frecuencia de 60 Hz
+		}
+
+		atenuacion = atenuacion + 0.04; // incrementamos los valores de la amplitud 4%.
+		if(atenuacion >= 1)
+		{
+			atenuacion = 1; //la magnitud maxima a alcanzar es de 100%
+		}
+
+	}
+	else
+	{
+//		generamos la rampa de frecuencia y voltaje en descenso (proceso de apagado)
+		f = f - step ;// incrementamos los valores de la frencuencia 10 Hz.
+		if(f <= 12)
+		{
+			f = 0; // si f alcanza una frecuencia de 120 Hz regresa a una frecuencia de 0 Hz
+			HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, RESET); // señal de que debemos apagar DC Link
+		}
+
+		atenuacion = atenuacion - 0.04; // reducimos los valores de la amplitud 4% en cada paso.
+		if(atenuacion <= 0.2)
+		{
+			atenuacion = 0; //la magnitud minima a alcanzar es de 20% y luego apagate
+		}
+
+	}
+
+
+}
 
 /* USER CODE END 4 */
 
